3a8b3f38fb8d8ca33bebd17e02d174d001c4a2a3
Muhammed Thasneem P Android <muhammed.thasneem@yahoo.com>
2017-11-12 12:04:37 +0530

    final code with glide

--- app/src/main/java/app/muhammed/com/imageloader/MainActivity.java
+++ app/src/main/java/app/muhammed/com/imageloader/MainActivity.java
@@ -2,12 +2,22 @@ package app.muhammed.com.imageloader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageView = findViewById(R.id.imageView);
+
+
+        Glide.with(this).load("http://www.vizteams.com/wp-content/uploads/2014/04/android.jpg").into(mImageView);
     }
 }
